38dbbeb6d928df9448ac6c49d49977ca7d68cc17
amiraghabaglu <95378130+amiraghabaglu@users.noreply.github.com>
2024-07-19 10:57:13 +0330

    Update model.py

--- model.py
+++ model.py
@@ -1,1 +1,20 @@
-mode
+#model
+class Users:
+    def __init__(self):
+        self.name = ""
+        self.pas = ""
+        self.mess = ""
+
+    def setName(self, uname):
+        self.name = uname
+    def setPass(self, passw):
+        self.pas = passw
+    def setMess(self, messa):
+        self.mess = messa
+
+    def getName(self):
+        return self.name
+    def getPass(self):
+        return self.pas
+    def getMess(self):
+        return self.mess
