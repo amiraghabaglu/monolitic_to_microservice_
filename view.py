b920f40d879ac8bf335829e9052c000ff456a6b2
amiraghabaglu <95378130+amiraghabaglu@users.noreply.github.com>
2024-07-21 00:39:39 +0330

    Update view.py

--- view.py
+++ view.py
@@ -1,11 +1,14 @@
-#view 
-from controller import AccountController
-from model import User
+from flask import render_template_string # type: ignore
 
-u = Users()
-ac = AccountController()
-
-u.setName(input("username"))
-u.setPass(input("password"))
-ac.login(u)
-print(u.getMess())
+def show_items(items):
+    template = """
+    <h1>Items List</h1>
+    <ul>
+  
+        <li>{{ items.get_fname() }} - ${{ items.get_lname() }}</li>
+        <li>{{ items.get_gender() }} - ${{ items.get_parentage() }}</li>
+        <li>{{ items.get_address() }}</li>
+    
+    </ul>
+    """
+    return render_template_string(template, items=items)
